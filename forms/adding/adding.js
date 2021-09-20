36dff148ee30fb7d554383f1dcc230c193ebcd06
Rachel Day <rnd94709@creighton.edu>
2021-09-20 15:50:27 -0500

    getting second number from user and multiplying

--- forms/adding/adding.js
+++ forms/adding/adding.js
@@ -4,3 +4,6 @@ console.log(`The sum is ${num1 + num2}`)
 
 let newNum = Number(prompt("Enter a whole number: "))
 console.log(`If you multiply the second number with the new number you get ${num2 * newNum}`)
+
+let newNum2 = Number(prompt("Enter a whole number: "))
+console.log(`If you multiply the second number with the new number you get ${num2 * newNum2}`)
