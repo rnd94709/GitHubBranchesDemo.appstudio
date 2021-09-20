fc6b4742892e72d6d1e03f1b6f2bff65fb341173
Rachel Day <rnd94709@creighton.edu>
2021-09-20 15:45:13 -0500

    get number from user and multiply

--- forms/adding/adding.js
+++ forms/adding/adding.js
@@ -1,3 +1,6 @@
 let num1 = 10
 let num2 = 20
 console.log(`The sum is ${num1 + num2}`)
+
+let newNum = Number(prompt("Enter a whole number: "))
+console.log(`If you multiply the second number with the new number you get ${num2 * newNum}`)
